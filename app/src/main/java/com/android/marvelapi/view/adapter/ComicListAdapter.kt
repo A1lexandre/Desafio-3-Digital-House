ac32ae2c5126b868036e05b9dd62eb8b8fdba6f6
Alexandre Araújo <a1lexandre.na@gmail.com>
2020-12-22 23:13:51 -0300

    Comic list item and recyclerView adapter

---
+++ app/src/main/java/com/android/marvelapi/view/adapter/ComicListAdapter.kt
@@ -0,0 +1,27 @@
+package com.android.marvelapi.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.android.marvelapi.databinding.ComicListItemBinding
+import com.android.marvelapi.model.MarvelComic
+
+class ComicListAdapter(val listOfComics: List<MarvelComic>): RecyclerView.Adapter<ComicListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ViewHolder(ComicListItemBinding.inflate(inflater, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(listOfComics[position])
+    }
+
+    override fun getItemCount(): Int = listOfComics.size
+
+    inner class ViewHolder(val view: ComicListItemBinding): RecyclerView.ViewHolder(view.root) {
+        fun bind(comic: MarvelComic) = with(view) {
+           // Glide.with(view.root.context).load()
+        }
+    }
+}
